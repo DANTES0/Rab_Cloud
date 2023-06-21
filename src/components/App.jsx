cf4756cd9f3764a22a74c98677a54f6c585780d8
Hjjik <vaaa@mail.ru>
2023-06-21 20:27:35 +0300

    version 5

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./navbar/Navbar";
+import './App.less';
+function App() {
+  return (
+    <div className='app'>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
